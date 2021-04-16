e21b356983e03968063b512314ad43b2e52c113f
dbr4fr <dbr4fr@outlook.com>
2021-04-16 18:16:08 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -109,7 +109,7 @@ function Install_ct() {
     mv gost-linux-"$bit"-"$ct_new_ver" gost
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
-    wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
+    wget --no-check-certificate https://raw.githubusercontent.com/dbr4fr/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
     mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   fi
 
